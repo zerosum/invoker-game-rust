d3cd528cf569c3eabf5a36543e917919c8d65373
zerosum <info@zerosum.dev>
2020-08-25 08:04:40 +0900

    can play on console!

--- site/index.js
+++ site/index.js
@@ -1,2 +1,8 @@
 const wasm = import("./node_modules/invoker-game-wasm/invoker_game_wasm");
-wasm.catch(console.error)
+
+wasm.then(wasm => {
+    window.addEventListener("keyup", (event) => {
+        wasm.update(event.keyCode);
+        console.log(wasm.fetch_status());
+    });
+});
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,25 +1,72 @@
 extern crate invoker_game_core as core;
+#[macro_use]
+extern crate lazy_static;
+extern crate serde;
 extern crate wasm_bindgen;
 
+use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
 
 use core::game::*;
 
-#[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
+mod ctx {
+    use std::sync::RwLock;
+
+    use core::game::*;
+
+    lazy_static! {
+        pub static ref PLAYER: RwLock<Invoker> = {
+            let p = Invoker::new();
+            RwLock::new(p)
+        };
+    }
 }
 
-#[wasm_bindgen(start)]
-pub fn run() {
-    let mut player = Invoker::new();
-    player.cast_element(Element::Exort);
-    player.cast_element(Element::Exort);
-    player.cast_element(Element::Exort);
-    player.invoke();
+#[derive(Serialize, Deserialize)]
+pub struct Player {
+    pub e1: char,
+    pub e2: char,
+    pub e3: char,
+    pub s1: &'static str,
+    pub s2: &'static str,
+}
 
-    log(player.spell1.map_or_else(|| "empty", display_spell))
+#[wasm_bindgen]
+pub fn fetch_status() -> JsValue {
+    let p = ctx::PLAYER.read().unwrap();
+
+    let _player = Player {
+        e1: display_element(p.elements[0]),
+        e2: display_element(p.elements[1]),
+        e3: display_element(p.elements[2]),
+        s1: p.spell1.map_or_else(|| "", display_spell ),
+        s2: p.spell2.map_or_else(|| "", display_spell ),
+    };
+
+    JsValue::from_serde(&_player).unwrap()
+}
+
+#[wasm_bindgen]
+pub fn update(key_code: u32) {
+    let mut p = ctx::PLAYER.write().unwrap();
+    match key_code {
+        81u32 => p.cast_element(Element::Quas),
+        87u32 => p.cast_element(Element::Wex),
+        69u32 => p.cast_element(Element::Exort),
+        82u32 => p.invoke(),
+        // 68u32 => {}
+        // 80u32 => {}
+        _ => {}
+    }
+}
+
+fn display_element(e: Element) -> char {
+    match e {
+        Element::Quas => 'Q',
+        Element::Wex => 'W',
+        Element::Exort => 'E',
+        Element::Void => ' ',
+    }
 }
 
 fn display_spell(s: Spell) -> &'static str {
